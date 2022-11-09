28e6002a23bc0d2407bbf9bb1494572338e342b5
Auditya Maulana Adnan <91017662+aud1tya4dnan0@users.noreply.github.com>
2022-11-09 10:54:39 +0700

    Update nomer3.cpp

--- nomer3.cpp
+++ nomer3.cpp
@@ -41,13 +41,13 @@ class Graph {
 int main(){
     Graph graph1(5);
 
-    graph1.addEdge( 0, 1);
-    graph1.addEdge( 0, 4);
-    graph1.addEdge( 1, 2);
-    graph1.addEdge( 1, 3);
-    graph1.addEdge( 1, 4);
-    graph1.addEdge( 2, 3);
-    graph1.addEdge( 3, 4);
+    graph1.addEdge( 0, 2);
+    graph1.addEdge( 0, 8);
+    graph1.addEdge( 1, 10);
+    graph1.addEdge( 1, 8);
+    graph1.addEdge( 1, 5);
+    graph1.addEdge( 2, 35);
+    graph1.addEdge( 3, 6);
 
     graph1.showGraph();
 
